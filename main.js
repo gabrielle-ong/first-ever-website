9e7cd709ce38f3c07ad8c63e128d0257c8fda09c
Gabrielle Ong Hui Min <gabrielleong97@gmail.com>
2017-02-08 01:17:47 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/* Really wanted to put this javascript in but i cant download jQuery	
+		var red = [o, 100, 63];
+		var orange = [40, 100, 60];
+		var green = [75, 100, 40];
+		var blue = [196, 77, 55];
+		var purple = [280, 50, 60];
+
+		var myName = "Gabrielle";
+		var letterColors = [red, orange, green, blue, purple];
+
+		if(myName.length < 10) {
+			var bubbleShape = "circle";
+			bounceBubbles();
+		}
+		else {
+			var bubbleShape = "square";
+		}
+			
+		drawName(myName, letterColors);
+*/
